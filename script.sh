52e317a9884431854b968bd44682fe8f7178e998
phamtasticle <38803222+phamtasticle@users.noreply.github.com>
2018-11-19 22:59:31 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+#Kevin Pham
+#This script is to test case the inputs
+#of a program and check to see if the output
+#is correct with the gold file.
+
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 echo "Test Run Undergrade $i Credits"
-~cgi_web/Tuition/cost <inputUG$i >temp
+~cgi_web/Tuition/cost <inputUG$i >temp #Get the input from a file, and store the outputin a temporary file
 
-cmp -s temp goldUG$i
+cmp -s temp goldUG$i #compare the file to the correct output file to make sure they have the same bytes
 if [ $? -eq 0 ];
 
 then 
 	echo "Test Pass"	
 else
 	echo "Test Failed"
-	cat temp goldUG$i  >>error.txt
+	cat temp goldUG$i  >>error.txt #store the error output and the correct output into a file
 	
 fi
 done
